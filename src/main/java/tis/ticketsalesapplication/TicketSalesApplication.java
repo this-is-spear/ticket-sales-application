cbc62a8161f7eb8258fb018ffa6cd9b6fd55f79e
tis <92219795+this-is-spear@users.noreply.github.com>
2022-05-26 10:34:01 +0900

    Initial commit

---
+++ src/main/java/tis/ticketsalesapplication/TicketSalesApplication.java
@@ -0,0 +1,13 @@
+package tis.ticketsalesapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tis/ticketsalesapplication/TicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package tis.ticketsalesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
